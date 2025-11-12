0b35b28d2ffae541e4dec157a99a118084f1b671
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-12 12:41:50 +0700

    nambahin import untuk file Navigasi.kt

---
+++ app/src/main/java/com/example/questnavigastugas_214/Navigasi.kt
@@ -0,0 +1,11 @@
+package com.example.questnavigastugas_214
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas_214.view.*
