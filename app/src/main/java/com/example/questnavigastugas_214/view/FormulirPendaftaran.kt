193aa3bd048dd4c4b75ec82da0f13038388da93e
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-12 12:53:10 +0700

    buat composable FormulirPendaftaran dengan parameter onSubmit

--- app/src/main/java/com/example/questnavigastugas_214/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_214/view/FormulirPendaftaran.kt
@@ -31,3 +31,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun FormulirPendaftaran(
+    modifier: Modifier = Modifier,
+    onSubmit: (String, String, String, String) -> Unit
+) {
+}
